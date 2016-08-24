ab6ba38d173cc758ec43370851ab8b3ccbbd59b4
Katie O'Neill <katiemarieoneill@gmail.com>
2016-08-23 21:32:52 -0700

    Add conditionals and results. Make answers booleans. Play with order of conditionals for accurate selection

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,11 +1,54 @@
 puts "What is your name?"
 	name = gets.chomp
+	if name == "Drake Cula"
+		name = false
+	elsif name == "Tu Fang"
+		name = false
+	else
+		name =true
+	end
+
 puts "How old are you?"
 	age = gets.chomp
 puts "What year were you born?"
-	year = gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	dob = gets.chomp.to_i
+	if (Time.now.year - dob) != age.to_i
+		age = false
+	else
+		age = true
+	end
+		
+puts "Our company cafeteria serves garlic bread. Should we order some for you?(yes/no)"
 	garlic = gets.chomp
-puts "Would you like to enroll in the company's health insurance?"
+	if garlic == "yes"
+		garlic = true
+	else 
+		garlic = false
+	end
+
+puts "Would you like to enroll in the company's health insurance?(yes/no)"
 	insurance = gets.chomp
+	if insurance == "yes"
+		insurance = true
+	else
+		insurance = false
+	end
+
+puts age, garlic, insurance, name 
+if !name
+	puts "Definitely a vampire"
+elsif age && garlic && insurance 
+	puts "Probably not a vampire."
+elsif !age && !garlic && !insurance
+	puts "Almost certainly a vampire"
+elsif !insurance || (!age && !garlic)
+	puts "Probably a vampire"
+# elsif !age && !garlic && !insurance
+# 	puts "Almost certainly a vampire"
+# elsif !name
+# 	puts "Definitely a vampire"
+else
+	puts "Results inconclusive"
+end
+	
 	
