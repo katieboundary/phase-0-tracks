e9800524b0c7acbb5e09e41199de06b00a56333c
Katie O'Neill <katiemarieoneill@gmail.com>
2016-08-31 23:07:50 -0700

    Create highway with cars and passengers to practice nested data structures

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,24 @@
+#Nested data structure of a freeway
+
+freeway = {
+	honda_civic:{
+		seat_info: {total_seats: 5, seats_available: 3},
+	
+		names_of_passengers: ["Elvira", "Frankenstein"]
+	},
+
+	toyota_corolla:{
+		seat_info: {total_seats: 5, seats_available: 1},
+
+		names_of_passengers: ["Dracula", "Nosferatu", "Hubert", "Brunhilda"]
+	},
+
+	hond_crx:{
+		seat_info: {total_seats: 2, seats_available: 1},
+
+		names_of_passengers: ["Han Solo"]
+	}
+
+}
+
+p freeway
