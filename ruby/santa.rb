94ac58c9ec1ed0671fc50dd69d5aaf004fdfc7c9
Katie O'Neill <katiemarieoneill@gmail.com>
2016-09-08 21:56:03 -0700

    Add methods for initialize, eating cookies, and speaking. Make new instance of santa

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,21 @@
 class Santa
 
+	def initialize
+		puts "Initializing Santa instance ..."
+	end
+
+	def speak
+		"Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies (cookie)
+		"That was a good #{cookie}"
+	end
 
 
 end
+
+santa = Santa.new
+p santa.speak
+p santa.eat_milk_and_cookies("snickerdoodle")
+
