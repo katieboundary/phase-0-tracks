3892eebd17f1c20886ac5aba9174b637c9271f81
Katie O'Neill <katiemarieoneill@gmail.com>
2016-09-10 20:09:10 -0700

    Create two shout methods to practice standalone modules using the keyword self

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,1 +1,14 @@
+module Shout
+  
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
 
+  def self.yelling_happily(words)
+  	words + "!" + ":)"
+  end
+
+end
+
+p Shout.yell_angrily("Argh")
+p Shout.yelling_happily("Weee")
