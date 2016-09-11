a692bf1b80dc7bc9d23d1f524fa52a55e4dca98b
Katie O'Neill <katiemarieoneill@gmail.com>
2016-09-10 23:26:29 -0700

    Write pseudocode for word guessing game module 6.6

---
+++ ruby/game/game.rb
@@ -0,0 +1,13 @@
+#Take in player one input (player two look away!). Don't print out what they entered. Can I hide what they entered? 
+#input: string, convert into array?
+#output:nothing or take up space to remove the word from the screen?
+#Tell player two how many guesses they have. (number of guesses is related to the length of the word.LENGTH)
+#output:string
+#Show how many letters are in the word "_ _ _ _"
+#input:string, convert into array?
+#output:"_ ..." /or "Guess again"
+#Repeated guesses don't count against them (IF user input is equal previous input then counter does not decrease)
+#When the user guesses a correct letter, it will fill. "a _ _ _"
+#input: string
+#output: see above
+#Give a congratulatory message upon winning and a taunting message if they lose.
