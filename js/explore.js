8fb4e43cd247a9b836a938544dba98bf7aba4c32
Katie O'Neill <katiemarieoneill@gmail.com>
2016-09-12 21:33:40 -0700

    Create program that reverses a string with driver code and a for loop

---
+++ js/explore.js
@@ -0,0 +1,18 @@
+//Create program to reverse a word in js. so "hello" would become "olleh".
+//take in string, set an empty new string variable, iterate through each letter and relocate with a loop.
+//return reversed word. 
+//call reversed word.
+
+
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+//Driver code:
+console.log(reverseString('hello'));
+
+
